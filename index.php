22c78d1d06d0be264abd71beea74db5ab98b2890
fx6-PC\fx6 <folex70@gmail.com>
2019-07-13 09:43:53 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+
+	<head>
+	
+	
+	
+	</head>
+
+	<body>
+		<div>	
+			<p>O QUE ACHOU DESSE PRODUTO?</p>
+			<div><p>5 Estrelas</p></div>
+			<div><p>4 Estrelas</p></div>
+			<div><p>3 Estrelas</p></div>
+			<div><p>2 Estrelas</p></div>
+			<div><p>1 Estrelas</p></div>
+		</div>
+	
+		<div>	
+			<p>MOSTRA PRA GENTE</p>
+			<p>Nos mostre o produto em ação</p>
+			<div><p>Escolher foto</p></div>
+		</div>
+		
+		<div>	
+			<p>FALE MAIS!</p>
+			<textarea placeholder="Nos diga o que achou do produto" ></textarea>
+			<div><p>Escolher foto</p></div>
+		</div>
+		
+		<div>	
+			<p>SOBRE VOCÊ!</p>
+			<input type="text" placeholder="Primeiro Nome" ></input>
+			<input type="text" placeholder="Segundo Nome" ></input>
+			<input  type="email" placeholder="Segundo Nome" ></input>
+			<div><button>Feito</button></div>
+		</div>
+	
+	</body>
+	
+</html>
